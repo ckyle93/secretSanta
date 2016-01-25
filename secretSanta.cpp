6c2ddd2b273f52ead9ceb342c7d600ea2be70996
Christopher <Ckyle@ucsc.edu>
2016-01-25 13:35:57 -0800

    Finish the functionality of the program

--- secretSanta.cpp
+++ secretSanta.cpp
@@ -1,11 +1,13 @@
 // By Chris Kyle
 // from listed input assign random secret santa partners
 // without assigning someone their relative.
+// https://www.reddit.com/r/dailyprogrammer/comments/3yiy2d/20151228_challenge_247_easy_secret_santa/
 
 #include <vector>
 #include <sstream>
 #include <iostream>
 #include <fstream>
+
 
 using namespace std;
 
@@ -24,7 +26,7 @@ class Person {
         // as a partner.
         vector<string> relatives;
         // The secret santa parter of this person.
-        string partner;
+        string partner = "I don't have a partner";
     public:
         // Don't allow default constructor, everyone has a name.
         Person() = delete;
@@ -36,17 +38,19 @@ class Person {
         // but do not include your own name as a relative.
         void add_relatives(vector<string>&);
         // Retrieves the vector of relatives.
+        string get_partner();
         vector<string> get_relatives();
         // Sets the partner variable, returns 0 if successful
         // and 1 if the argument is the name of the person.
         int give_partner(string);
+        void print_person(ostream&);
 };
 
 Person::Person(string _name) : name{_name} {}
 
 string Person::get_name() { return name; }
 
-void Person::add_relatives(vector<string>& names) {
+void Person::add_relatives(Lvector<string>& names) {
     for (auto _name : names) {
         if (name != _name){
             relatives.push_back(_name);
@@ -54,15 +58,25 @@ void Person::add_relatives(vector<string>& names) {
     }
 }
 
+string Person::get_partner() { return partner; }
+
 vector<string> Person::get_relatives() { return relatives; }
 
-int Person::give_partner(string _name) { 
-    if (name == _name) {
-        return 1;
-    } else {
-        partner = _name;
+int Person::give_partner(string _name) {
+    if (name == _name){
         return 0;
     }
+    for (auto relative : relatives) {
+        if (_name == relative) {     // If we try to add a relative
+            return 0;               // as a partner, return 0.
+        }
+    }
+    partner = _name;
+    return 1;
+}
+
+void Person::print_person(ostream& out) {
+    out << name << " " << partner << endl;
 }
 
 int main() {
@@ -92,11 +106,28 @@ int main() {
     vector<Person> noPartner = people;
     vector<Person> noReceive = people; // No one has been assigned the people
                                        // on this list.
-    for (person : noPartner) {
+    vector<Person> badSolution;
+    for (auto person = noPartner.begin(); person != noPartner.end();) {
         int x = rand() % noReceive.size();
-        while (person.give_partner(noReceive.at(x)));
-        
-// If you do not haven't been assigned a partner yet, then you are in
+        while (true) {
+            if ((*person).give_partner((noReceive.at(x)).get_name())) {
+                badSolution.push_back(*person);
+                break;
+            } else {
+                x = rand() % noReceive.size();
+            }
+        }
+        noPartner.erase(person);
+        // ++person;
+        auto itor = noReceive.begin() + x;
+        noReceive.erase(itor);
+    }
+
+    for (auto person : badSolution) {
+        person.print_person(cout);
+    }
+    return 0;
+// If you haven't been assigned a partner yet, then you are in
 // the noPartner vector. For every member of the noPartner vector,
 // assign them a partner from the noReceive vector. The give_partner
 // function will return a 1 if that partner is a relative, or a 0 if a valid
@@ -116,4 +147,5 @@ vector<string> split(const string& s, char delim) {
     }
     return elems;
 }
+
 
